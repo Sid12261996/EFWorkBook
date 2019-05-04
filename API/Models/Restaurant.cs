ceccb7413ebdc97c555dc9b5e38364e4d331061e
sid12261996 <sidharthrkc@gmail.com>
2019-05-04 14:59:11 +0530

    First Commit for CRUD

---
+++ API/Models/Restaurant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name  { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ API/Models/RestaurantDbConttext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class RestaurantDbConttext:DbContext
+    {
+      public DbSet<Restaurant> Restaurants { get; set; }
+       
+
+    }
+}
